f66aa7dbcb28a06efdc0e4b87f35edca7e93b5bd
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2024-09-18 09:12:09 +0530

    Update task.router.js

--- routers/task.router.js
+++ routers/task.router.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { getUserTasks, filter } = require('../controllers/task.controller');
+const { getUserTasks } = require('../controllers/task.controller');
 
 router.get('/tasks/:userId', getUserTasks);
-router.get('/filter', filter);
 
 module.exports = router;
